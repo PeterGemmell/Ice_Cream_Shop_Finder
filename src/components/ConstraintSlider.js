308a58a82a1a53cdc3d52e8a7f3a5a048f40b38c
Peter Gemmell <petergemmell65@hotmail.com>
2020-06-02 13:58:51 +0100

    constraint slider component for distance by car created

---
+++ src/components/ConstraintSlider.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Icon, Slider } from 'antd';
+
+
+const ConstraintSlider = (({iconType, value, onChange text}) => {
+  return (
+    <section className= "d-flex flex-column">
+    <div className= "d-flex w-100 align-items-center">
+    <Icon className="font-1-5 mr-4" type={iconType} />
+    <Slider className="w-100" value={value} min={0} max={60} onChange={onChange} />
+    </div>
+    <span className="text-center">{text}</span>
+    </section>
+  );
+});
+
+export default ConstraintSlider;
+
+// Here what is created is a car Icon and slider which can be moved in order
+// to select the distance in minutes the ice cream shop is away by car.
