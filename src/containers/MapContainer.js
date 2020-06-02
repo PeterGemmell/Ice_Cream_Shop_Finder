bc0e5cfa28bad04b27b587221bbcde1c1d2d3e65
Peter Gemmell <petergemmell65@hotmail.com>
2020-06-02 17:26:05 +0100

    now working on render section

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -88,8 +88,75 @@ apiHasLoaded = ((map, mapsApi) => {
 
 // With the constraints, find some places serving ice-cream.
 handleSearch = (() => {
-  const { }
-})
+  const { markers, constraints, placesService, directionService, mapsApi } = this.state;
+  if(markers.length === 0){
+    message.warn('Add a constraint and try again!');
+    return;
+  }
+  const filteredResults = [];
+  const marker = markers[0];
+  const timeLimit = constraints[0].time;
+  const markerLatLng = new mapsApi.LatLng(marker.lat, marker.lng);
+
+  const placesRequest = {
+    location: markerLatLng,
+    type: ['restaurant', 'cafe'],
+    query: 'ice cream',
+    rankBy: mapsApi.places.rankBy.DISTANCE,
+  };
+
+  // First, search for ice cream shops.
+  placesService.textSearch(placesRequest, ((response) => {
+  // Only look at the nearest top 5.
+  const responseLimit = Math.min(5, response.length);
+  for(let i=0; i < responseLimit; i++){
+    const iceCreamPlace = response[i];
+    const { rating, name } = iceCreamPlace;
+    const address = iceCreamPlace.formated_address; // for example 5 Buchanan Street, Glasgow.
+    const priceLevel = iceCreamPlace.price_level;
+    let photoUrl = '';
+    let openNow = false;
+    if(iceCreamPlace.opening_hours){
+      openNow = iceCreamPlace.opening_hours.open_now; // e.g true or false is it open.
+    }
+    if(iceCreamPlace.photos && iceCreamPlace.photos.length > 0){
+      photoUrl = iceCreamPlace.photos[0].getUrl();
+    }
+
+    // Second, For each iceCreamPlace, check if it is within acceptable travelling distance.
+    const directionRequest = {
+      origin: markerLatLng,
+      destination: address, // Address of the ice cream place.
+      travelMode: 'DRIVING',
+    }
+    directionService.route(directionRequest, ((result, status) => {
+      if(status !== 'OK') { return }
+      const travellingRoute = result.routes[0].legs[0]; // { duration: { text: 1mins, value: 600 } }
+      const travellingTimeInMinutes = travellingRoute.duration.value / 60;
+      if(travellingTimeInMinutes < timeLimit){
+        const distanceText = travellingRoute.distance.text; //for example 10kms away.
+        const timeText = travellingRoute.duration.text; //for example its 15 mins away.
+        filteredResults.push({
+          name,
+          rating,
+          address,
+          openNow,
+          priceLevel,
+          photoUrl,
+          distanceText,
+          timeText,
+        });
+      }
+      // Finally, Add results to state.
+      this.setState({ searchResults: filteredResults });
+    }));
+  }
+ }));
+});
+
+render(){
+  
+}
 
 
 
